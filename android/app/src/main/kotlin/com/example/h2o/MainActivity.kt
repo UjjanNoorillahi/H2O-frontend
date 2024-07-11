f8d8933b52591353b47f93205b5229756a789d09
Noor Illahi <ujjannoorillahi@gmail.com>
2024-07-11 20:06:12 +0500

    event section

--- android/app/src/main/kotlin/com/example/h2o/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.h2o
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ h2o_mobile_app/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:h2o/provider/auth_token_provider.dart';
+import 'package:h2o/screens/home_screen/home_screen.dart';
+import 'package:h2o/screens/splash_screen/splash_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  // Ensure that Flutter binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = 'pk_test_51Oda4PHSvDuMR6pwhSgqNrMgZNSlmr4LUGSGwPSuUpG7ns3YltEjeTW7oOIGOkKk8EmY7yt8MnxRXzhRin0sxqcR0045cbxygI';
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) async {
+    final prefs = await SharedPreferences.getInstance();
+    final userJsonString = prefs.getString('data');
+
+    runApp(MyApp(
+      initialRoute: userJsonString != null ? 'home' : 'login',
+    ));
+  });
+}
+
+class MyApp extends StatelessWidget {
+  final String initialRoute;
+
+  const MyApp({
+    super.key,
+    required this.initialRoute,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => AuthTokenProvider()),
+        ],
+        child: CupertinoApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: const CupertinoThemeData(
+            // Set a light theme explicitly
+            brightness: Brightness.light,
+            // Add other theme properties as needed
+          ),
+          home: initialRoute == 'login' ? SplashScreen() : Home(),
+        ));
+  }
+}
+
